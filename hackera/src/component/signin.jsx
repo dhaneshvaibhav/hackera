470fae8509caac1fcef203dbededc46252c774c7
saikirandamerla <154958470+saik2422@users.noreply.github.com>
2025-02-26 11:55:38 +0530

    edited log in page

--- hackera/src/component/signin.jsx
+++ hackera/src/component/signin.jsx
@@ -5,16 +5,20 @@ function Signin() {
     const [rep ,setrep]=useState("")
     const [password ,setPasswprd]=useState("")
     return (  <>
+    <h1>Create An Account:</h1>
+    <p>Email:</p>
     <input
     type="text"
     value={user}
     placeholder="username or email"
     onChange={(e)=>setUser(e.target.value)}/>
+    <p>Password:</p>
     <input
     type="text"
     value={password}
     placeholder="password"
     onChange={(e)=>setPasswprd(e.target.value)}/>
+    <p>Repeat password:</p>
     <input
     type="text"
     value={rep}

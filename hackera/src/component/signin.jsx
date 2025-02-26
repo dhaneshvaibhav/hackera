306f0162e2f12a86d6b8bc1e05d57167d2108b38
saikirandamerla <154958470+saik2422@users.noreply.github.com>
2025-02-26 12:23:43 +0530

    edited sign in page

--- hackera/src/component/signin.jsx
+++ hackera/src/component/signin.jsx
@@ -4,28 +4,35 @@ function Signin() {
     const [user ,setUser]=useState("")
     const [rep ,setrep]=useState("")
     const [password ,setPasswprd]=useState("")
-    return (  <>
-    <h1>Create An Account:</h1>
-    <p>Email:</p>
-    <input
-    type="text"
-    value={user}
-    placeholder="username or email"
-    onChange={(e)=>setUser(e.target.value)}/>
-    <p>Password:</p>
-    <input
-    type="text"
-    value={password}
-    placeholder="password"
-    onChange={(e)=>setPasswprd(e.target.value)}/>
-    <p>Repeat password:</p>
-    <input
-    type="text"
-    value={rep}
-    placeholder="repeat password"
-    onChange={(e)=>setrep(e.target.value)}/>
-    <button>signin</button>
-    </>);
+    return (  <div className="login-box">
+        <h1 className="login-title">Create An Account:</h1>
+  
+        <p>Email:</p>
+        <input
+          type="text"
+          value={user}
+          placeholder="username or email"
+          onChange={(e) => setUser(e.target.value)}
+        />
+  
+        <p>Password:</p>
+        <input
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+  
+        <p>Repeat password:</p>
+        <input
+          type="password"
+          value={rep}
+          placeholder="repeat password"
+          onChange={(e) => setRep(e.target.value)}
+        />
+  
+        <button>Sign In</button>
+      </div>);
 }
 
 export default Signin;
